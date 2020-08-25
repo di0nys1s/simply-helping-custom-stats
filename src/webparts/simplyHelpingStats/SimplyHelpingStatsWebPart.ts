28e83adc848567e56faa7e9ca79307be672b012d
Burak Seyhan <seyhanburak9@gmail.com>
2020-08-25 12:52:41 +1000

    initial commit

---
+++ src/webparts/simplyHelpingStats/SimplyHelpingStatsWebPart.ts
@@ -0,0 +1,61 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+    BaseClientSideWebPart,
+    IPropertyPaneConfiguration,
+    PropertyPaneTextField
+  } from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import * as strings from 'SimplyHelpingStatsWebPartStrings';
+
+// Importing Vue.js
+import Vue from 'vue';
+// Importing Vue.js SFC
+import SimplyHelpingStatsComponent from './components/SimplyHelpingStats.vue';
+
+export interface ISimplyHelpingStatsWebPartProps {
+  description: string;
+}
+
+export default class SimplyHelpingStatsWebPart extends BaseClientSideWebPart<ISimplyHelpingStatsWebPartProps> {
+
+  public render(): void {
+    const id: string = `wp-${this.instanceId}`;
+    this.domElement.innerHTML = `<div id="${id}"></div>`;
+
+    let el = new Vue({
+      el: `#${id}`,
+      render: h => h(SimplyHelpingStatsComponent, {
+        props: {
+          description: this.properties.description
+        }
+      })
+    });
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/simplyHelpingStats/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISimplyHelpingStatsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SimplyHelpingStatsWebPartStrings' {
+  const strings: ISimplyHelpingStatsWebPartStrings;
+  export = strings;
+}
