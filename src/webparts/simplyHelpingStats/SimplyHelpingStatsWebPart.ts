bc0c28653447fdc49fd2547c6f0e68b8dd1edf3b
Burak Seyhan <seyhanburak9@gmail.com>
2020-08-25 19:30:09 +1000

    boxes and table ui

--- src/webparts/simplyHelpingStats/SimplyHelpingStatsWebPart.ts
+++ src/webparts/simplyHelpingStats/SimplyHelpingStatsWebPart.ts
@@ -1,61 +1,31 @@
-import { Version } from '@microsoft/sp-core-library';
-import {
-    BaseClientSideWebPart,
-    IPropertyPaneConfiguration,
-    PropertyPaneTextField
-  } from '@microsoft/sp-webpart-base';
-import { escape } from '@microsoft/sp-lodash-subset';
-
-import * as strings from 'SimplyHelpingStatsWebPartStrings';
-
+// import { Version } from '@microsoft/sp-core-library';
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
 // Importing Vue.js
-import Vue from 'vue';
+import Vue from "vue";
 // Importing Vue.js SFC
-import SimplyHelpingStatsComponent from './components/SimplyHelpingStats.vue';
+import SimplyHelpingStatsComponent from "./components/SimplyHelpingStats.vue";
 
 export interface ISimplyHelpingStatsWebPartProps {
+  msg: string;
   description: string;
 }
 
-export default class SimplyHelpingStatsWebPart extends BaseClientSideWebPart<ISimplyHelpingStatsWebPartProps> {
-
+export default class SimplyHelpingStatsWebPart extends BaseClientSideWebPart<
+  ISimplyHelpingStatsWebPartProps
+> {
   public render(): void {
     const id: string = `wp-${this.instanceId}`;
     this.domElement.innerHTML = `<div id="${id}"></div>`;
 
     let el = new Vue({
       el: `#${id}`,
-      render: h => h(SimplyHelpingStatsComponent, {
-        props: {
-          description: this.properties.description
-        }
-      })
+      render: h =>
+        h(SimplyHelpingStatsComponent, {
+          props: {
+            msg: "This message is coming from TS file",
+            description: this.properties.description
+          }
+        })
     });
-  }
-
-  protected get dataVersion(): Version {
-    return Version.parse('1.0');
-  }
-
-  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-    return {
-      pages: [
-        {
-          header: {
-            description: strings.PropertyPaneDescription
-          },
-          groups: [
-            {
-              groupName: strings.BasicGroupName,
-              groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
-    };
   }
 }
--- src/webparts/simplyHelpingStats/components/SimplyHelpingStats.vue
+++ src/webparts/simplyHelpingStats/components/SimplyHelpingStats.vue
@@ -1,128 +1,255 @@
 <template>
-  <!--
-  <div :class="$style.SimplyHelpingStats">
-    <div :class="$style.container">
-      <div :class="$style.row">
-        <div :class="$style.column">
-          <span :class="$style.title">Welcome!</span>
-          <p :class="$style.subTitle">Customize SharePoint experiences using Web Parts.</p>
-          <p :class="$style.description">{{description}}</p>
-          <a href="https://aka.ms/spfx" :class="$style.button">
-            <span :class="$style.label">Learn more</span>
-          </a>
-        </div>
+  <div class="container">
+    <div class="box-container">
+      <div class="box box-1">
+        <h1>10</h1>
+        <p>Active Franchise sites</p>
+      </div>
+      <div class="box box-2">
+        <h1>3</h1>
+        <p>Inactive Franchise Sites</p>
+      </div>
+      <div class="box box-3">
+        <h1>1</h1>
+        <p>Non-use last 30 days</p>
+      </div>
+      <div class="box box-4">
+        <h1>1.5<span id="gb-used">Gb used</span></h1>
+        <p>Franchise storage utilised</p>
       </div>
     </div>
-  </div>
-  -->
-  <div>
-  <h1>Simply Helping Site Stats</h1>
+
+    <div class="table-responsive stats-table">
+      <table class="table table-hover table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">Franchise</th>
+            <th scope="col">Status</th>
+            <th scope="col">Storage Utilised</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="table-warning"><a href="#">Malley</a></td>
+            <td class="table-warning">Needs attention, inactive last 45 days</td>
+            <td class="table-warning">0.2 Gb</td>
+          </tr>
+          <tr>
+            <td class="table-success"><a href="#">Bayswater</a></td>
+            <td class="table-success">OK - using Franchise site</td>
+            <td class="table-success">0.4 Gb</td>
+          </tr>
+          <tr>
+            <td class="table-danger"><a href="#">Brunswick</a></td>
+            <td class="table-danger">Urgent - No activity at risk</td>
+            <td class="table-danger">0.2 Gb</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <h1>Hello {{ fullName }}</h1>
+    <h3>{{ welcome }}</h3>
+    <p>{{ msg }}</p>
+    <p>{{ description }}</p>
+    <h5>{{ counter }}</h5>
+    <button class="increment" @click="incrementCounter()">Increment</button>
+    <button @click="decrementCounter()">Decrement</button>
   </div>
 </template>
 
-      
-    
-  
+<script lang="ts">
+import Vue from "vue";
+import SimplyHelpingStatsWebPart from "../SimplyHelpingStatsWebPart";
+
+const addStyleSheet = function(fileURL) {
+  var file = document.createElement("link");
+  file.rel = "stylesheet";
+  file.type = "text/css";
+  file.href = fileURL;
+  document.head.appendChild(file);
+};
+
+const addScript = function(fileURL, defer = false) {
+  var tag = document.createElement("script");
+  tag.src = fileURL;
+  if (defer) {
+    tag.setAttribute("defer", "");
+  }
+  document.head.appendChild(tag);
+};
+
+addStyleSheet(
+  "//stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+);
+addScript("//cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js");
+
+import jQuery from "jquery";
+let $ = jQuery;
+
+$("p").css("color", "red");
+
+export default Vue.extend({
+  name: "Hello",
+  props: {
+    msg: String,
+    description: String
+  },
+  data() {
+    return {
+      welcome: "Welcome to Simply Helping Site Stats",
+      counter: 0,
+      firstName: "Burak",
+      lastName: "Seyhan"
+    };
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
+    },
+    decrementCounter() {
+      this.counter--;
+    }
+  },
+  computed: {
+    fullName: {
+      get: function() {
+        return this.firstName + " " + this.lastName;
+      }
+    }
+  }
+});
+</script>
+
+<!--
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 
 /**
  * Component's properties
  */
+/*
 export interface ISimplyHelpingStatsProps {
   description: string;
 }
+*/
 
 /**
  * Class-component
  */
 @Component
-export default class SimplyHelpingStats extends Vue
-  implements ISimplyHelpingStatsProps {
+export default class SimplyHelpingStats extends Vue {
+  // implements ISimplyHelpingStatsProps {
   /**
    * implementing ISimpleWebPartProps interface
    */
-  @Prop()
-  public description: string;
+
+  @Prop() public description: string;
+  @Prop() private msg!: string;
+  firstName = 'Burak'
+  lastName = 'Seyhan'
+  counter = 0
+
+  mounted() {
+    console.log('This is mounted log');
+  }
+
+  get fullName() {
+    return this.firstName +  ' ' + this.lastName;
+  } 
+
+  incrementCounter() {
+    this.counter++;  
+  }
+
 }
 </script>
+-->
 
-<style lang="scss">
-@import "~@microsoft/sp-office-ui-fabric-core/dist/sass/_SPFabricCore.scss";
+<style scoped>
+.container {
+  margin: 0px auto;
+  padding: 10px 20px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+}
 
-/*
-.SimplyHelpingStats {
-  .container {
-    max-width: 700px;
-    margin: 0px auto;
-    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+@media only screen and (max-width: 500px) {
+  .box-container {
+    grid-template-areas:
+      "box-1"
+      "box-2"
+      "box-3"
+      "box-4";
   }
 
-  .row {
-    @include ms-Grid-row;
-    @include ms-fontColor-white;
-    background-color: $ms-color-themeDark;
-    padding: 20px;
-  }
-
-  .column {
-    @include ms-Grid-col;
-    @include ms-lg10;
-    @include ms-xl8;
-    @include ms-xlPush2;
-    @include ms-lgPush1;
-  }
-
-  .title {
-    @include ms-font-xl;
-    @include ms-fontColor-white;
-  }
-
-  .subTitle {
-    @include ms-font-l;
-    @include ms-fontColor-white;
-  }
-
-  .description {
-    @include ms-font-l;
-    @include ms-fontColor-white;
-  }
-
-  .button {
-    // Our button
-    text-decoration: none;
-    height: 32px;
-
-    // Primary Button
-    min-width: 80px;
-    background-color: $ms-color-themePrimary;
-    border-color: $ms-color-themePrimary;
-    color: $ms-color-white;
-
-    // Basic Button
-    outline: transparent;
-    position: relative;
-    font-family: "Segoe UI WestEuropean", "Segoe UI", -apple-system,
-      BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
-    -webkit-font-smoothing: antialiased;
-    font-size: $ms-font-size-m;
-    font-weight: $ms-font-weight-regular;
-    border-width: 0;
-    text-align: center;
-    cursor: pointer;
-    display: inline-block;
-    padding: 0 16px;
-
-    .label {
-      font-weight: $ms-font-weight-semibold;
-      font-size: $ms-font-size-m;
-      height: 32px;
-      line-height: 32px;
-      margin: 0 4px;
-      vertical-align: top;
-      display: inline-block;
-    }
+  .box {
+    width: 150px;
+    height: auto;
   }
 }
-*/
+
+@media only screen and (min-width: 501px) {
+  .box-container {
+    column-gap: 20px;
+    grid-template-areas:
+      "box-1 box-2"
+      "box-3 box-4";
+  }
+
+  .box {
+    width: 250px;
+    height: auto;
+  }
+}
+
+.box-container {
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  row-gap: 20px;
+  margin-top: 10px;
+}
+
+.box {
+  background-color: #eee;
+  color: black;
+  text-align: center;
+}
+
+.box h1 {
+  font-size: 60px;
+  padding: 0;
+  margin: 10px 0;
+}
+
+.box p {
+  margin: 0 0 15px 0;
+}
+
+.box-1 {
+  grid-area: box-1;
+}
+.box-2 {
+  grid-area: box-2;
+  background-color: #ff7a98;
+}
+.box-3 {
+  grid-area: box-3;
+  background-color: #ffb787;
+}
+.box-4 {
+  grid-area: box-4;
+}
+
+#gb-used {
+  font-size: 10px;
+}
+
+.stats-table {
+  margin-top: 20px;
+}
+
+.stats-table a {
+  text-decoration: none;
+}
 </style>
